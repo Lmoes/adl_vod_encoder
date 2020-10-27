0b32fb21b6d92cce038da84f816cf5029649bb41
lmoesing <leander.moesinger@geo.tuwien.ac.at>
2020-10-27 17:38:43 +0100

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for adl_vod_encoder.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
