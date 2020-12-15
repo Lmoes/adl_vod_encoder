cdf9242bf5012fd4bcb257dee56bc23ac3a8d03e
lmoesing <leander.moesinger@geo.tuwien.ac.at>
2020-12-15 19:17:04 +0100

    update tests

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -5,7 +5,11 @@ from src.adl_vod_encoder.data_io.vod_data_loaders import VodDataset, VodTempPrec
 import os
 import numpy as np
 
+
 class TestDatasets(unittest.TestCase):
+    """
+    Test whether the dataloaders work as expected
+    """
     def setUp(self):
         testdatadir = os.path.join(os.getcwd(), 'testdata')
         VodDatasetpath = os.path.join(testdatadir, 'v01_erafrozen_k_weeklysample.nc')
@@ -15,22 +19,33 @@ class TestDatasets(unittest.TestCase):
 
     def test_vod_equality(self):
         """
-        Test whether both loaders load the same vod data
+        Test whether both loaders load the same vod data.
         :return:
         """
         dataA = self.vod_ds.data[~np.isnan(self.vod_ds.data)]
         dataB = self.era5_ds.data[~np.isnan(self.era5_ds.data)]
         self.assertEqual(dataA.tolist(), dataB.tolist(), 'test datasets are not read equally')
 
-    def test_std(self):
-        self.assertAlmostEqual(np.nanstd(self.vod_ds.data), 1, delta=0.1, msg='standardized vod values have a variance != 1')
-        self.assertAlmostEqual(np.nanstd(self.era5_ds.precdata), 1, delta=0.1, msg='standardized prec values have a variance != 1')
-        self.assertAlmostEqual(np.nanstd(self.era5_ds.tempdata), 1, delta=0.1, msg='standardized temp values have a variance != 1')
+    """
+    Check if datasets have mean==0 and std==1 after standardization.
+    """
+    def test_std_vod(self):
+        self.assertAlmostEqual(np.nanstd(self.vod_ds.data), 1, delta=0.2, msg='standardized vod values have a variance != 1')
 
-    def test_mean(self):
-        self.assertAlmostEqual(np.nanmean(self.vod_ds.data), 0, delta=0.1, msg='standardized vod values have a mean != 0')
-        self.assertAlmostEqual(np.nanmean(self.era5_ds.precdata), 0, delta=0.1, msg='standardized prec values have a mean != 0')
-        self.assertAlmostEqual(np.nanmean(self.era5_ds.tempdata), 0, delta=0.1, msg='standardized temp values have a mean != 0')
+    def test_std_precipitation(self):
+        self.assertAlmostEqual(np.nanstd(self.era5_ds.precdata), 1, delta=0.2, msg='standardized prec values have a variance != 1')
+
+    def test_std_temperature(self):
+        self.assertAlmostEqual(np.nanstd(self.era5_ds.tempdata), 1, delta=0.2, msg='standardized temp values have a variance != 1')
+
+    def test_mean_vod(self):
+        self.assertAlmostEqual(np.nanmean(self.vod_ds.data), 0, delta=0.2, msg='standardized vod values have a mean != 0')
+
+    def test_mean_precipitation(self):
+        self.assertAlmostEqual(np.nanmean(self.era5_ds.precdata), 0, delta=0.2, msg='standardized prec values have a mean != 0')
+
+    def test_mean_temperature(self):
+        self.assertAlmostEqual(np.nanmean(self.era5_ds.tempdata), 0, delta=0.2, msg='standardized temp values have a mean != 0')
 
 
 if __name__ == '__main__':
